8c97f1cb6c75ae2de05bddecf70da92a8dcd1b9e
LuisaNiar <luisaniar@unisabana.edu.co>
2024-05-15 13:50:29 -0500

    Eclat Commit

--- recommender.py
+++ recommender.py
@@ -8,6 +8,36 @@ class Recommender:
     """
     
     def train(self, prices, database) -> None:
+
+        def eclat(P, minsup, F, prefix):
+            for Xa, t_Xa in P.items():
+                if len(t_Xa) >= minsup:
+                    F.append((prefix + [Xa], len(t_Xa)))
+                    Pa = {}
+                    for Xb, t_Xb in P.items():
+                        if Xb > Xa:
+                            t_Xab = [transaction for transaction in t_Xa if transaction in t_Xb]
+                            if len(t_Xab) >= minsup:
+                                Pa[Xb] = t_Xab
+                    if Pa:
+                        eclat(Pa, minsup, F, prefix + [Xa])
+
+        # Definir el umbral mínimo de soporte
+        minsup = 100
+
+        # Inicializar P con los ítems únicos y sus transacciones
+        P = {}
+        for transaction in database:
+            for item in transaction:
+                if item in P:
+                    P[item].append(transaction)
+                else:
+                    P[item] = [transaction]
+
+        # Calcular los itemsets frecuentes utilizando el algoritmo 
+        F = []
+        eclat(P, minsup, F, [])
+       
         """
             allows the recommender to learn which items exist, which prices they have, and which items have been purchased together in the past
             :param prices: a list of prices in USD for the items (the item ids are from 0 to the length of this list - 1)
