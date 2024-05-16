2184695090b50cdbe6b2cdce9e84fbed0acdc350
AndresBA <110069945+AndresFelipeBonillaAvila@users.noreply.github.com>
2024-05-16 16:02:01 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,28 +1,22 @@
 import numpy as np
 import itertools
 from collections import defaultdict
-from multiprocessing import Pool, cpu_count
 
 class Recommender:
-    def __init__(self, minsup=0.2, min_confidence=0.1):
-        self.minsup = minsup
-        self.min_confidence = min_confidence
-
     def train(self, prices, database) -> None:
-        def eclat(P, minsup, prefix, F, num_transactions):
-            for Xa, t_Xa in P.items():
-                support_Xa = len(t_Xa)
-                if support_Xa >= minsup:
-                    rsup_Xa = support_Xa / num_transactions
-                    F.append((prefix + [Xa], support_Xa, rsup_Xa))
-                    Pa = {Xb: t_Xa & t_Xb for Xb, t_Xb in P.items() if Xb > Xa}
-                    if Pa:
-                        eclat(Pa, minsup, prefix + [Xa], F, num_transactions)
+        def eclat(itemsets, minsup, prefix, frequent_itemsets, num_transactions):
+            for item, transactions in itemsets.items():
+                support_count = len(transactions)
+                if support_count >= minsup:
+                    relative_support = support_count / num_transactions
+                    frequent_itemsets.append((prefix + [item], support_count, relative_support))
+                    new_itemsets = {item_next: transactions & transactions_next for item_next, transactions_next in itemsets.items() if item_next > item}
+                    if new_itemsets:
+                        eclat(new_itemsets, minsup, prefix + [item], frequent_itemsets, num_transactions)
 
-        def generate_association_rules(frequent_itemsets, min_confidence, prices, database_length):
-            def process_itemset(args):
-                itemset, support, rsup = args
-                rules = []
+        def generate_association_rules(frequent_itemsets, min_confidence):
+            rules = []
+            for itemset, support, relative_support in frequent_itemsets:
                 if len(itemset) > 1:
                     for i in range(1, len(itemset)):
                         for antecedent in itertools.combinations(itemset, i):
@@ -32,43 +26,45 @@ class Recommender:
                                 confidence = support / antecedent_support
                                 consequent_rsup = get_rsup(frequent_itemsets, consequent)
                                 lift = confidence / consequent_rsup
-                                leverage = rsup - (antecedent_support / database_length * consequent_rsup)
-                                profits = sum(prices[item_id] for item_id in consequent)
+                                leverage = relative_support - (antecedent_support / len(database) * consequent_rsup)
+
+                                profits = calculate_profits(consequent, prices)
+
                                 if confidence >= min_confidence and leverage > 0 and lift > 1:
                                     rules.append((antecedent, consequent, profits, confidence, lift, leverage))
-                return rules
-
-            def get_support(frequent_itemsets, itemset):
-                itemset_set = set(itemset)
-                for fi, support, _ in frequent_itemsets:
-                    if set(fi) == itemset_set:
-                        return support
-                return 0
-
-            def get_rsup(frequent_itemsets, itemset):
-                itemset_set = set(itemset)
-                for fi, _, rsup in frequent_itemsets:
-                    if set(fi) == itemset_set:
-                        return rsup
-                return 0
-
-            args = [(itemset, support, rsup) for itemset, support, rsup in frequent_itemsets]
-            with Pool(cpu_count()) as pool:
-                results = pool.map(process_itemset, args)
-                rules = [rule for sublist in results for rule in sublist]
             return rules
 
-        minsup_absolute = max(1, int(self.minsup * len(prices)))
+        def calculate_profits(consequent, prices):
+            return sum(prices[item_id] for item_id in consequent)
 
-        P = defaultdict(set)
-        for tid, transaction in enumerate(database):
+        def get_support(frequent_itemsets, itemset):
+            itemset_set = set(itemset)
+            for fi, support, _ in frequent_itemsets:
+                if set(fi) == itemset_set:
+                    return support
+            return 0
+
+        def get_rsup(frequent_itemsets, itemset):
+            itemset_set = set(itemset)
+            for fi, _, rsup in frequent_itemsets:
+                if set(fi) == itemset_set:
+                    return rsup
+            return 0
+
+        minsup = max(1, int(0.2 * len(prices)))
+        min_confidence = 0.1
+
+        item_transactions = defaultdict(set)
+        for transaction_id, transaction in enumerate(database):
             for item in transaction:
-                P[item].add(tid)
+                item_transactions[item].add(transaction_id)
 
         num_transactions = len(database)
-        F = []
-        eclat(P, minsup_absolute, [], F, num_transactions)
-        rules = generate_association_rules(F, self.min_confidence, prices, num_transactions)
+
+        frequent_itemsets = []
+        eclat(item_transactions, minsup, [], frequent_itemsets, num_transactions)
+
+        rules = generate_association_rules(frequent_itemsets, min_confidence)
 
         self.rules = rules
         self.prices = prices
@@ -84,6 +80,11 @@ class Recommender:
                 for item in consequent:
                     if item not in cart_set:
                         recommendations[item] += lift
+
+        sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
+        recommended_items = [item for item, _ in sorted_recommendations[:max_recommendations]]
+
+        return recommended_items
 
         sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
         recommended_items = [item for item, _ in sorted_recommendations[:max_recommendations]]
