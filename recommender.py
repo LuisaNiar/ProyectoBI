88cb1c1761ccd42052acece611afb133942abd9c
LuisaNiar <84819474+LuisaNiar@users.noreply.github.com>
2024-05-16 17:49:05 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,58 +1,65 @@
 import numpy as np
 import itertools
+import pandas as pd
 from collections import defaultdict
-from multiprocessing import Pool, cpu_count
+
 
 class Recommender:
+
     def train(self, prices, database) -> None:
-        def eclat(P, minsup, prefix, F, num_transactions):
+        def eclat(P, minsup, prefix, F):
+            num_transactions = len(database)
             for Xa, t_Xa in P.items():
                 support_Xa = len(t_Xa)
+                rsup_Xa = support_Xa / num_transactions
                 if support_Xa >= minsup:
-                    rsup_Xa = support_Xa / num_transactions
                     F.append((prefix + [Xa], support_Xa, rsup_Xa))
-                    Pa = {Xb: t_Xa & t_Xb for Xb, t_Xb in P.items() if Xb > Xa}
+                    Pa = {}
+                    for Xb, t_Xb in P.items():
+                        if Xb > Xa:
+                            t_Xab = t_Xa & t_Xb
+                            support_Xab = len(t_Xab)
+                            rsup_Xab = support_Xab / num_transactions
+                            if support_Xab >= minsup:
+                                Pa[Xb] = t_Xab
                     if Pa:
-                        eclat(Pa, minsup, prefix + [Xa], F, num_transactions)
+                        eclat(Pa, minsup, prefix + [Xa], F)
 
         def generate_association_rules(frequent_itemsets, min_confidence):
-            def process_itemset(itemset_support_rsup):
-                itemset, support, rsup = itemset_support_rsup
-                rules = []
+            rules = []
+            for itemset, support, rsup in frequent_itemsets:
                 if len(itemset) > 1:
                     for i in range(1, len(itemset)):
                         for antecedent in itertools.combinations(itemset, i):
                             consequent = tuple(sorted(set(itemset) - set(antecedent)))
                             if consequent:
                                 antecedent_support = get_support(frequent_itemsets, antecedent)
-                                confidence = support / antecedent_support
+                                rule_support = support
+                                confidence = rule_support / antecedent_support
                                 consequent_rsup = get_rsup(frequent_itemsets, consequent)
                                 lift = confidence / consequent_rsup
-                                leverage = rsup - (antecedent_support / len(database) * consequent_rsup)
-                                profits = calculate_profits(consequent, prices)
-                                if confidence >= min_confidence and leverage > 0 and lift > 1:
-                                    rules.append((antecedent, consequent, profits, confidence, lift, leverage))
-                return rules
+                                leverage = rsup - (get_rsup(frequent_itemsets, antecedent) * consequent_rsup)
 
-            with Pool(cpu_count()) as pool:
-                results = pool.map(process_itemset, frequent_itemsets)
-                rules = [rule for sublist in results for rule in sublist]
+                                # Calculate profits using antecedent, consequent, and prices
+                                profits = calculate_profits(consequent, prices)
+
+                                if 1 > confidence >= min_confidence and leverage > 0 and lift > 1:
+                                    rules.append((antecedent, consequent, profits, confidence, lift, leverage))
             return rules
 
         def calculate_profits(consequent, prices):
-            return sum(prices[item_id] for item_id in consequent)
+            consequent_prices = [prices[item_id] for item_id in consequent]
+            return sum(consequent_prices)
 
         def get_support(frequent_itemsets, itemset):
-            itemset_set = set(itemset)
             for fi, support, _ in frequent_itemsets:
-                if set(fi) == itemset_set:
+                if set(fi) == set(itemset):
                     return support
             return 0
 
         def get_rsup(frequent_itemsets, itemset):
-            itemset_set = set(itemset)
             for fi, _, rsup in frequent_itemsets:
-                if set(fi) == itemset_set:
+                if set(fi) == set(itemset):
                     return rsup
             return 0
 
@@ -66,31 +73,56 @@ class Recommender:
             for item in transaction:
                 P[item].add(tid)
 
-        num_transactions = len(database)
-
         # Calcular los itemsets frecuentes utilizando el algoritmo Eclat
         F = []
-        eclat(P, minsup, [], F, num_transactions)
+        eclat(P, minsup, [], F)
 
         # Generar reglas de asociación a partir de los itemsets frecuentes
         rules = generate_association_rules(F, min_confidence)
 
+        # Convertir el conjunto de itemsets frecuentes a un DataFrame
+        df_frequent_itemsets = pd.DataFrame(F, columns=['Itemset', 'Support', 'Relative Support'])
+
+        # Imprimir el DataFrame
+        print("Conjunto de itemsets frecuentes:")
+        print(df_frequent_itemsets)
+
+        # Convertir el conjunto de reglas a un DataFrame
+        df_rules = pd.DataFrame(rules,
+                                columns=['Antecedent', 'Consequent', 'Profits', 'Confidence', 'Lift', 'Leverage'])
+
+        # Filtrar reglas con leverage negativo
+        df_rules = df_rules[df_rules['Leverage'] >= 0]
+
+        # Imprimir el DataFrame de reglas filtrado
+        print("Reglas de asociación:")
+        print(df_rules)
+
+        # Do something
         self.rules = rules
         self.prices = prices
+
+        # Return this object again
         return self
 
     def get_recommendations(self, cart: list, max_recommendations: int) -> list:
-        recommendations = defaultdict(float)
-        cart_set = set(cart)
 
+        recommendations = defaultdict(float)
+
+        # Find rules where the cart items are in the antecedent
         for rule in self.rules:
             antecedent, consequent, profits, confidence, lift, leverage = rule
-            if set(antecedent).issubset(cart_set):
+            if set(antecedent).issubset(set(cart)):
                 for item in consequent:
-                    if item not in cart_set:
-                        recommendations[item] += lift
+                    recommendations[item] += lift
 
+        # Sort recommendations by lift (or any other measure) and return the top ones
         sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
         recommended_items = [item for item, _ in sorted_recommendations[:max_recommendations]]
+
+        print("Sorted Recommendations:")
+        print(sorted_recommendations)
+        print("Recommended Items:")
+        print(recommended_items)
 
         return recommended_items
