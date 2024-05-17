67d790a4830a31d383e72b06f2133820e691c2e7
AndresBA <110069945+AndresFelipeBonillaAvila@users.noreply.github.com>
2024-05-17 16:05:08 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -10,21 +10,21 @@ class Recommender:
                 rsup_Xa = support_Xa / num_transactions
                 if rsup_Xa > 0:  # Filtrar elementos con soporte relativo 0
                     if support_Xa >= minsup:
-                        F.append((prefix + [Xa], support_Xa, rsup_Xa))
+                        F.append((prefix + [Xa], rsup_Xa))
                         Pa = {Xb: t_Xa & t_Xb for Xb, t_Xb in P.items() if Xb > Xa}
                         if Pa:
                             eclat(Pa, minsup, prefix + [Xa], F, num_transactions)
 
         def generate_association_rules(frequent_itemsets, min_confidence):
             rules = []
-            for itemset, support, rsup in frequent_itemsets:
+            for itemset, rsup in frequent_itemsets:
                 if len(itemset) > 1:
                     for i in range(1, len(itemset)):
                         for antecedent in itertools.combinations(itemset, i):
                             consequent = tuple(sorted(set(itemset) - set(antecedent)))
                             if consequent:
-                                antecedent_support = get_support(frequent_itemsets, antecedent)
-                                confidence = support / antecedent_support
+                                antecedent_support = get_rsup(frequent_itemsets, antecedent)
+                                confidence = rsup / antecedent_support
                                 consequent_rsup = get_rsup(frequent_itemsets, consequent)
                                 lift = confidence / consequent_rsup
 
@@ -33,7 +33,7 @@ class Recommender:
 
                                 if confidence >= min_confidence and lift > 1:
                                     combined_metric = confidence + lift
-                                    rules.append((antecedent, consequent, profits, confidence, lift, conviction, combined_metric))
+                                    rules.append((antecedent, consequent, profits, confidence, lift, conviction, combined_metric, rsup))
             # Ordenar las reglas primero por ganancias y luego por la suma de confianza y lift de mayor a menor
             rules.sort(key=lambda x: (x[2], x[6]), reverse=True)
             return rules
@@ -41,16 +41,9 @@ class Recommender:
         def calculate_profits(consequent, prices):
             return sum(prices[item_id] for item_id in consequent)
 
-        def get_support(frequent_itemsets, itemset):
-            itemset_set = set(itemset)
-            for fi, support, _ in frequent_itemsets:
-                if set(fi) == itemset_set:
-                    return support
-            return 0
-
         def get_rsup(frequent_itemsets, itemset):
             itemset_set = set(itemset)
-            for fi, _, rsup in frequent_itemsets:
+            for fi, rsup in frequent_itemsets:
                 if set(fi) == itemset_set:
                     return rsup
             return 0
@@ -79,15 +72,15 @@ class Recommender:
         self.frequent_itemsets = F
 
         # Imprimir itemsets frecuentes
-        print(f"{'Itemset':<20} {'Support':<10} {'Relative Support':<10}")
-        for itemset, support, rsup in self.frequent_itemsets:
-            print(f"{str(itemset):<20} {support:<10} {rsup:<10.2f}")
+        print(f"{'Itemset':<20} {'Relative Support':<10}")
+        for itemset, rsup in self.frequent_itemsets:
+            print(f"{str(itemset):<20} {rsup:<10.2f}")
 
         # Imprimir reglas de asociación
-        print(f"{'Antecedent':<20} {'Consequent':<20} {'Profit':<10} {'Confidence':<10} {'Lift':<10} {'Conviction':<10} {'Confidence + Lift':<10}")
+        print(f"{'Antecedent':<20} {'Consequent':<20} {'Profit':<10} {'Confidence':<10} {'Lift':<10} {'Conviction':<10} {'Confidence + Lift':<10} {'Relative Support':<10}")
         for rule in self.rules:
-            antecedent, consequent, profits, confidence, lift, conviction, combined_metric = rule
-            print(f"{str(antecedent):<20} {str(consequent):<20} {profits:<10.2f} {confidence:<10.2f} {lift:<10.2f} {conviction:<10.2f} {combined_metric:<10.2f}")
+            antecedent, consequent, profits, confidence, lift, conviction, combined_metric, rsup = rule
+            print(f"{str(antecedent):<20} {str(consequent):<20} {profits:<10.2f} {confidence:<10.2f} {lift:<10.2f} {conviction:<10.2f} {combined_metric:<10.2f} {rsup:<10.2f}")
 
         return self
 
@@ -96,7 +89,7 @@ class Recommender:
         cart_set = set(cart)
 
         for rule in self.rules:
-            antecedent, consequent, profits, confidence, lift, conviction, combined_metric = rule
+            antecedent, consequent, profits, confidence, lift, conviction, combined_metric, rsup = rule
             if set(antecedent).issubset(cart_set):
                 for item in consequent:
                     if item not in cart_set:
