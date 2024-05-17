1d0e36dc4214e0f63c2920a6a9ae4141ef596d91
AndresBA <110069945+AndresFelipeBonillaAvila@users.noreply.github.com>
2024-05-17 16:15:01 -0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -100,7 +100,7 @@ class Recommender:
             if set(antecedent).issubset(cart_set):
                 for item in consequent:
                     if item not in cart_set:
-                        recommendations[item] += combined_metric
+                        recommendations[item] += support
 
         sorted_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)
         recommended_items = [item for item, _ in sorted_recommendations[:max_recommendations]]
